674c0da9313364ee5ecad75559a824518932ab86
aline-guttner <aline.guttner@gmail.com>
2022-07-27 17:33:15 -0300

    commit on master

---
+++ src/dados/pets.tsx
@@ -0,0 +1,21 @@
+import { v4 as uuidv4 } from 'uuid';
+
+let Pets = [
+    {
+        "nome": "Gatildo",
+        "src": "assets/imagens/Gatildo.jpg",
+        "id": uuidv4
+    },
+    {
+        "nome": "Bichano",
+        "src": "assets/imagens/Bichano.jpeg",
+        "id": uuidv4
+    },
+    {
+        "nome": "Porcão",
+        "src": "assets/imagens/Porcão.jpeg",
+        "id": uuidv4
+    }
+]
+
+export default Pets;
--- src/pages/Perfil/Principal/index.tsx
+++ src/pages/Perfil/Principal/index.tsx
@@ -1,12 +1,29 @@
 import style from './Principal.module.scss';
 import tamara from 'assets/imagens/tamara-andreeva-priroda-peizazh-gory-altai-zhivotnoe-kot.jpg'
+import classNames from 'classnames';
+import pets from 'dados/pets'
+import userPic from 'assets/imagens/User.png'
 
 export default function Principal() {
     return (
 
-        <section className={style.principal}>
-            <h2>Dados</h2>
+        <section className={classNames({
+            [style.principal]: true,
+            "container": true
+        })}>
             <img src={tamara} className="img-fluid" alt="foto de gato olhando para paisagem montanhosa" />
+            <div className={style.fotosNomes}>
+                <span className={style.fotosNomes__user}>
+                    <img src={userPic} alt="Foto do usuário" />
+                    <p>Fulano de Tal</p>
+                </span>
+                {pets.map(pet => (
+                    <span className={style.fotosNomes__pets}>
+                        <img src={pet.src} className={style.fotosNomes__pets__fotosPets} alt="Foto de animal" />
+                        <p className={style.fotosNomes__pets__nomesPets}>{pet.nome}</p>
+                    </span>
+                ))}
+            </div>
         </section>
 
     )
