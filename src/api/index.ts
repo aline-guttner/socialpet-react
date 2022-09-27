f6a56b131a89d73afcd70e992b66682d55266293
aline-guttner <aline.guttner@gmail.com>
2022-09-27 11:59:23 -0300

    commit on 'master'

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-    baseURL: 'http://localhost:8000/'
-}); 
+    baseURL: 'https://socialpet-backend.herokuapp.com/'
+});
 
 export default http; 
