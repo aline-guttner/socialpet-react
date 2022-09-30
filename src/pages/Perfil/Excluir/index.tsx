350d91ae04f189bb3ee6f16c435e71bcbd28bcbc
aline-guttner <aline.guttner@gmail.com>
2022-09-29 21:56:43 -0300

    commit on 'master'

--- src/pages/Perfil/Excluir/index.tsx
+++ src/pages/Perfil/Excluir/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import style from './Excluir.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function Excluir() {
     const [excluindo, setExcluindo] = useState(false)
@@ -12,13 +13,17 @@ export default function Excluir() {
         setOverflow('hidden')
     }
 
+    let navigate = useNavigate();
+    const routeChange = () => {
+        navigate('../../');
+    }
 
     return (
         <>
             <button className={style.excluir} onClick={excluir}>Deletar conta</button>
             {excluindo && <div className={style.popup}>
                 <p>Tem certeza que deseja deletar sua conta? Você não poderá recuperá-lá depois.</p>
-                <span><button>Deletar</button><button onClick={() => { setExcluindo(false); setOverflow('visible') }}>Cancelar</button></span>
+                <span><button onClick={routeChange}>Deletar</button><button onClick={() => { setExcluindo(false); setOverflow('visible') }}>Cancelar</button></span>
             </div>}
         </>
     )
