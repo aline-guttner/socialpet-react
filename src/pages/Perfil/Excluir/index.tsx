df68411586ff6dd8e4f26d8ed75074de5ed7bf85
aline-guttner <aline.guttner@gmail.com>
2022-09-29 21:38:37 -0300

    commit on 'master'

--- src/pages/Perfil/Excluir/index.tsx
+++ src/pages/Perfil/Excluir/index.tsx
@@ -3,15 +3,22 @@ import style from './Excluir.module.scss';
 
 export default function Excluir() {
     const [excluindo, setExcluindo] = useState(false)
+    const [overflow, setOverflow] = useState('visible')
+
+    document.body.style.overflow = overflow
+
     const excluir = () => {
         setExcluindo(true)
+        setOverflow('hidden')
     }
+
+
     return (
         <>
             <button className={style.excluir} onClick={excluir}>Deletar conta</button>
             {excluindo && <div className={style.popup}>
                 <p>Tem certeza que deseja deletar sua conta? Você não poderá recuperá-lá depois.</p>
-                <span><button>Deletar</button><button>Cancelar</button></span>
+                <span><button>Deletar</button><button onClick={() => { setExcluindo(false); setOverflow('visible') }}>Cancelar</button></span>
             </div>}
         </>
     )
