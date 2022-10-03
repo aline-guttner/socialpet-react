d463bcdae7bc0522d5973376bdbebf51d1b6b385
Evandro Sacramento <99098957+3vandro@users.noreply.github.com>
2022-10-03 00:05:58 -0300

    Add files via upload

--- src/pages/AtualizaFeed/index.tsx
+++ src/pages/AtualizaFeed/index.tsx
@@ -1,12 +1,18 @@
-import { useEffect } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import style from '../Feed/Feed.module.scss';
 import axios from 'axios';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from "yup";
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate} from 'react-router-dom';
+import { appendFile } from 'fs';
+import  { baseURL } from 'api';
+import { Form } from 'react-bootstrap';
 
-
+interface IPost {
+    title: string,
+    content: string
+}
 
 const validationPost = yup.object().shape({
     title: yup.string().required("O título é obrigatório").max(40,),
@@ -15,45 +21,81 @@ const validationPost = yup.object().shape({
 })
 
 export default function EditarPost() {
+    const navigate = useNavigate()
     const { _id } = useParams()
-    let navigate = useNavigate()
+    const [ post, setPost] = useState<IPost>({
+        title:"",
+        content: ""
+    })
 
-    // falta colocar o id do usuario no caminho
-    const addPost = (data: Object) => axios.put(`https://socialpet-backend.herokuapp.com/user/user.${_id}/post/${_id}`, data)
+    useEffect(() => {
+        if(_id !== undefined){
+            findPost(_id)
+        }
+    },[_id] )
+
+    function updatePost (e: ChangeEvent<HTMLInputElement>) {
+
+        setPost({
+            ...post,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    
+
+    async function OnSubmit (e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+
+        const response = await axios.patch(`${baseURL}posts/${_id}`,post)
         .then(() => {
+            console.log("foi")
             navigate("/")
         })
         .catch(() => {
             console.log("deu erro")
         })
+    }
 
-    const { register, handleSubmit, formState: { errors }, reset } = useForm({
-        resolver: yupResolver(validationPost)
-    })
-
-    useEffect(() => {
-        axios.get(`https://socialpet-backend.herokuapp.com/posts/${_id}`)
-            .then((response) => {
-                reset(response.data)
-            })
-    }, [])
-
+    async function findPost (_id: string) {
+        const response = await axios.get(`${baseURL}posts/${_id}`)
+        setPost({
+            title: response.data.title,
+            content: response.data.content
+        })
+        console.log(response)
+    }
 
     return (
         <>
             <main className='container'>
                 <section>
-                    <form onSubmit={handleSubmit(addPost)} className={style.formPostagem}>
+                    <form  onSubmit={OnSubmit} className={style.formPostagem}>
                         <div>
                             <label htmlFor='titulo'>Título</label>
-                            <input type="text" id="titulo" {...register("title")} />
+                            <input 
+                            type="text" 
+                            id="titulo"  
+                            name="title"
+                            value={post.title}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatePost(e)}
+                            />
                         </div>
                         <br />
-                        <div>
+                        <Form.Group>
                             <label htmlFor='content'>Conteúdo</label>
-                            <textarea id='conteudo' className={style.inputConteudo} rows={10} {...register("content")} ></textarea>
+                            <Form.Control 
+                            as="textarea"
+                            id='conteudo' 
+                            className={style.inputConteudo}
+                            name="content"
+                            rows={10} 
+                            value={post.content}
+                            onChange={(e: ChangeEvent<HTMLInputElement>) => updatePost(e)}
+                            />
+                        </Form.Group>
 
-                        </div>
+                        
                         <br />
                         <button type='submit' className={style.largeButton} >Atualizar</button>
                     </form>
