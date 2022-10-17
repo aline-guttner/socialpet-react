150b2d7823f5fe7818e2d1f9787c79a9d9c7de27
Evandro Sacramento <99098957+3vandro@users.noreply.github.com>
2022-10-17 14:54:57 -0300

    Add files via upload

--- src/router.js
+++ src/router.js
@@ -1,27 +1,45 @@
 import Feed from './pages/Feed';
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, HashRouter, redirect } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 import Perfil from './pages/Perfil';
 import PaginaPadrao from 'components/PaginaPadrao';
 import Login from 'pages/Login';
 import Cadastro from 'pages/Cadastro';
 import { UserContextProvider } from 'contexts/UserContext';
 import EditarPost from 'pages/AtualizaFeed';
+import { isAuthenticated } from 'contexts/Auth';
+import { Outlet } from 'react-router-dom';
+
+
+
+
+
+const PrivateRoute = () => {
+    const auth = isAuthenticated(); 
+    return auth ? <Outlet /> : <Navigate to="/" />;
+}
+
+
 
 export default function AppRouter() {
+
+
+
     return (
         <HashRouter>
-            <Routes>
-
+           <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/user" element={<PaginaPadrao />} >
-                    <Route path="feed/:id" index element={<Feed />} />
-                    <Route path="perfil/:id" element={<Perfil />} />
+                <Route path="/user" element={<PrivateRoute />}>
+                    <Route path="/user" element={<PaginaPadrao />}>
+                            <Route path="feed/:id" index element={<Feed />} />
+                            <Route path="perfil/:id" element={<Perfil />} />
+                    </Route>
                 </Route>
+                    <Route exact path="/editarpost/:id" element={<EditarPost />} />
                 <Route path="/cadastro" element={<Cadastro />} />
-                <Route exact path="/editarpost/:id" element={<EditarPost />} />
-
             </Routes>
         </HashRouter>
     )
-}
 
+
+}
