acad1f86c5c9f681a1ca397d96b3067cdf9f94c1
aline-guttner <aline.guttner@gmail.com>
2022-10-18 01:00:59 -0300

    commit on 'master'

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const baseURL =
-    'http://localhost:8000'
-// 'https://socialpet-backend.herokuapp.com/'
+    // 'http://localhost:8000'
+    'https://socialpet-backend.herokuapp.com/'
 
 export const http = axios.create({
     baseURL:
-        'http://localhost:8000'
-    // 'https://socialpet-backend.herokuapp.com/'
+        // 'http://localhost:8000'
+        'https://socialpet-backend.herokuapp.com/'
 });
 
 //eu mudei o lugar da deletePostRequest pra PostContext, porque eu não tava conseguindo usar o mutate(), que faz o feed atualizar automaticamente, no arquivo da api. Tá lá no fim do arquivo.
